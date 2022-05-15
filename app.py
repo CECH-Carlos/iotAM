6041954d686ecf349e11252c67898c987171a9d1
Carlos <61739345+Carlos-CECH@users.noreply.github.com>
2022-05-14 21:40:42 -0300

    Inserted treat image function

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ class IotAm:
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        
+
         return bil_gaussian_img
 
     def read_cur(self, lan, imagem):            
@@ -65,37 +65,3 @@ class IotAm:
 
 win = IotAm()
 win.Iniciar()                                               
-
-#img = cv2.imread('Curriculo.jpg')
-#cv2.imshow('img', img)
-
-#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#cv2.imshow('cinza', gray)
-
-#_, bin = cv2.threshold(gray, 90, 255, cv2.THRESH_BINARY)
-#cv2.imshow('bin', bin)
-
-#desfoque = cv2.GaussianBlur(bin, (5,5), 0)
-#cv2.imshow('des', desfoque)
-
-#TODO: Ver sobre contornos de texto
-#_, contornos, hier = cv2.findContours(desfoque, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-#cv2.drawContours(img, contornos, -1, (0, 255, 0), 2)
-#cv2.imshow('cont', img)
-
-"""for c in contornos:
-
-    perimetro = cv2.arcLength(c, True)
-    aprox = cv2.approxPolyDP(c, 0.03 * perimetro, True)
-
-    if len(aprox) == 4:
-        (x, y, altura, largura) = cv2.boundingRect(c)
-        cv2.rectangle(img, (x, y), (x + altura, y + largura), (0, 255, 0), 2)
-        curriculo = img[y:y + largura, x:x + altura]
-        cv2.imwrite('currilo2.jpg', curriculo)"""
-
-#cv2.imshow('draw', img)
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
