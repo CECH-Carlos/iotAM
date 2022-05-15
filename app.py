85348df0489afcee502b9c3313f8fe8bb4a36ade
Carlos <61739345+Carlos-CECH@users.noreply.github.com>
2022-05-15 14:09:07 -0300

    app.py finished

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python 
 #-*- coding: utf-8 -*-
+import time
+import gtts
+from playsound import playsound
 import pytesseract
 from PySimpleGUI import PySimpleGUI as sg
 import cv2
@@ -8,7 +11,10 @@ class IotAm:
     def __init__(self):
         sg.theme('Reddit')
         layout = [
-            [sg.Text('Lingua:', size=(10, 1)), 
+            [sg.Text('Selecione a lingua em que o CV está escrito.', font='21')],
+            [sg.Text('Select the language in which the CV is written.', font='21')],
+            [sg.Text('Seleccione el idioma en el que está escrito el CV.', font='21')],
+            [sg.Text('Lingua:', font='21', size=(10, 1)), 
             sg.Combo(values=list(["português", "english", "spanish"]), key='lingua', default_value="português", size=(20, 1))],
             [sg.Button('Confirmar')]
         ]
@@ -25,8 +31,8 @@ class IotAm:
                 lan = self.get_lang(valores)
                 imagem = self.treat_image()
                 text = self.read_cur(lan, imagem)
-                self.save_cur(text)
-                break
+                arq = self.save_cur(text)
+                return arq
 
     def get_lang(self, valores):
         if valores['lingua'].lower() == 'english':
@@ -37,22 +43,25 @@ class IotAm:
             return 'spa'
         
     def treat_image(self):
-        imagem = cv2.imread("./assets/Curriculo.JPG")
-        imgH, imgW, _ = imagem.shape
-        bil_gaussian_img = cv2.bilateralFilter(imagem, 9, 75, 75)
-        cinza = cv2.cvtColor(bil_gaussian_img, cv2.COLOR_BGR2GRAY)
-        ret, mask = cv2.threshold(cinza, 100, 255, cv2.THRESH_BINARY)
+        imagem = cv2.imread("./assets/Curriculo_por.JPEG", 0)
+        imgW = imagem.shape[1]
+        imgH = imagem.shape[0]
+        #bil_gaussian_img = cv2.bilateralFilter(imagem, 9, 15, 15)
+        #cinza = cv2.cvtColor(bil_gaussian_img, cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(imagem, 56, 255, cv2.THRESH_BINARY)
+        mask2 = cv2.adaptiveThreshold(mask, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 7)
+        print(imagem.shape)
         print(ret)
         proporcao = float(imgH/imgW)
         new_Width = 620
         new_Height = int(new_Width*proporcao)
-        new_Image =  cv2.resize(mask, (new_Width, new_Height), interpolation=cv2.INTER_AREA)
+        new_Image =  cv2.resize(mask2, (new_Width, new_Height), interpolation=cv2.INTER_AREA)
         cv2.imshow('Curriculo', new_Image)
         
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        return mask
+        return mask2
 
     def read_cur(self, lan, imagem):            
         caminho = r"C:\Program Files\Tesseract-OCR"
@@ -67,7 +76,17 @@ class IotAm:
                 arquivo.write(text)
 
                 print('Currículo salvo!')
-                
 
-win = IotAm()
-win.Iniciar()                                               
+        return 'curriculo.txt'
+
+def speak_voice(arq):
+    with open(arq, 'r') as arquivo:
+        for linha in arquivo:
+            curriculo = gtts.gTTS(linha, lang='pt-br')
+            curriculo.save('curriculo.mp3')
+            playsound('curriculo.mp3')
+
+iot = IotAm()
+arq = iot.Iniciar()
+time.sleep(1)
+speak_voice(arq)
